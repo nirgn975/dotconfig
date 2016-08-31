468936442573fb65194ae8d3cbeb3114347a170f
Nir Galon <nirgn975@gmail.com>
2016-08-31 15:22:44 +0300

    Start writing some scripts and configuration files.

---
+++ .bash_profile
@@ -0,0 +1,31 @@
+###
+# Completion…
+###
+
+if [[ -n "$ZSH_VERSION" ]]; then  # quit now if in zsh
+    return 1 2> /dev/null || exit 1;
+fi;
+
+# bash completion.
+if  which brew > /dev/null && [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ]; then
+    source "$(brew --prefix)/share/bash-completion/bash_completion";
+elif [ -f /etc/bash_completion ]; then
+    source /etc/bash_completion;
+fi;
+
+# homebrew completion
+if  which brew > /dev/null; then
+    source "$(brew --prefix)/etc/bash_completion.d/brew"
+fi;
+
+# Enable tab completion for `g` by marking it as an alias for `git`
+if type __git_complete &> /dev/null; then
+    __git_complete g __git_main
+fi;
+
+
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/paulirish/google-cloud-sdk/path.bash.inc'
+
+# The next line enables shell command completion for gcloud.
+source '/Users/paulirish/google-cloud-sdk/completion.bash.inc'
---
+++ new-machine.sh
@@ -0,0 +1,60 @@
+# Essential..
+xcode-select --install
+
+# First install brew - we can't do anything in this world without a package manager.
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Now, let's save you some serious time.
+# install latest git.
+brew install git
+
+# Install the latest Ruby.
+brew install ruby
+
+# Install the latest rubygems.
+
+
+# Install latest Jekyll and Jekyll paginate.
+sudo gem install jekyll jekyll-paginate
+
+# Install latest SASS.
+sudo gem install sass
+
+# Install Tree - you need it, I know..
+brew install tree
+
+# Install ZSH and zsh completions.
+brew install zsh zsh-completions
+
+# Install thefuck.
+brew install thefuck
+
+# Update git to the latest version.
+brew install git
+
+# Instal latest version of Python 3.
+brew install python3
+
+# Install pip - python package manager.
+
+
+# Install PostgreSQL.
+brew install postgres
+
+# Install Node.js.
+brew install node
+
+# Install MongoDB.
+brew install mongodb
+
+# Install RethinkDB.
+brew install rethinkdb
+
+# Install `ng` command.
+npm install -g angular-cli
+
+# Install `electron` command.
+npm install -g electron
+
+# Install `gulp` command.
+npm install -g gulp
